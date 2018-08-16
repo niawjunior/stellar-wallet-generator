f2c3a14316a1146839ee316625d94b21b4eee673
niawjunior <pasupol@dome.cloud>
2018-08-16 17:45:12 +0700

    fix

--- functions/index.js
+++ functions/index.js
@@ -5,20 +5,24 @@ var Keypair = require("stellar-base").Keypair;
 const app = express();
 const cors = require('cors');
 
-app.use(cors({ origin: true }));
+app.use(cors({
+    origin: true
+}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-createAccount=()=> {
-    var newAccount = Keypair.random();    
+createAccount = () => {
+    var newAccount = Keypair.random();
     var account = {
         publicKey: newAccount.publicKey(),
         secret: newAccount.secret()
     }
-    return account
+    return account;
 }
 app.get('/wallet', (req, res) => {
-    res.status(200).send(createAccount())
+    res.status(200).send(createAccount());
 })
 // app.listen(3000, () => {
 //     console.log('Server Listening on http://localhost:3000/wallet');
